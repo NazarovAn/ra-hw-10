ac0ddfd337c338e9a6d81099a27310e36a8aff93
NazarovAn <naatonchrome@gmail.com>
2021-01-14 00:52:32 +0400

    Second task functional filter component done

--- src/components/HW-10.2/Filter.js
+++ src/components/HW-10.2/Filter.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Filter.css';
+import ServiceAdd from './components/ServiceAdd';
+import ServiceList from './components/ServiceList';
+import ServiceFilter from './components/ServiceFilter';
+// import ServiceAddClassBased from './components/ServiceAddClassBased';
+// import ServiceListClassBased from './components/ServiceListClassBased';
+import store from './store/store';
+import { Provider } from 'react-redux';
 
 export default function Filter() {
   return (
-    <div>
-      Фильтрация
-    </div>
+    <Provider store={ store }>
+      <div className="pd_1">
+        <ServiceAdd />
+        <ServiceList />
+        {/* <ServiceAddClassBased />
+        <ServiceListClassBased /> */}
+        <ServiceFilter />
+      </div>
+    </Provider>
   )
 }
